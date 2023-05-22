51b925d5c71aea6ca1e9550de65ff457512eff18
MikolajMichon <134232852+MikolajMichon@users.noreply.github.com>
2023-05-22 08:42:48 -0500

    Add files via upload

---
+++ Procos/Procos/ProcosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcosApp.swift
+//  Procos
+//
+//  Created by Mikolaj M. Michon on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ProcosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
